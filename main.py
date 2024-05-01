b69a03bdb53c68b6a75ee46c54a49b2b4b7f794a
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-05-01 14:30:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,49 +5,116 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 import time
 import re
+import ssl
+
+from flask import Response
+import json
 
 # Initialiser Flask
 app = Flask(__name__)
 
-# Définir la route pour la page d'accueil
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def filter_results(results):
+    unique_results = []
+    seen_urls = set()
+    valid_emails = []
+
+    for result in results:
+        # Vérifier si l'URL est unique
+        if result["url"] not in seen_urls:
+            seen_urls.add(result["url"])
+            unique_results.append(result)
+
+        # Vérifier le format de l'adresse e-mail
+        if re.match(r'[^@]+@[^@]+\.[^@]+', result["email"]):
+            valid_emails.append(result["email"])
+
+    return unique_results, valid_emails
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Définir la route pour le scraping
+
 @app.route('/scrape')
 def scrape():
-    # Récupérer la valeur du champ texte
     search_query = request.args.get('search_query')
-    # Construire l'URL de recherche Google Maps
-    link = f"https://www.google.com/maps/search/{search_query}" if search_query else "https://www.google.com/maps/search/agence+web+biarritz"
+    link = f"https://www.google.com/maps/search/{search_query}" 
+    chrome_options = Options()
+    chrome_options.add_argument('--ignore-ssl-errors=true')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    browser = webdriver.Chrome(options=chrome_options)
 
-    # Initialiser le webdriver Chrome
-    browser = webdriver.Chrome()
-    # Liste pour stocker les résultats
+# Liste pour stocker les résultats
     results = []
 
-    # Fonction pour extraire les données avec Selenium
+    def extract_emails(page_content):
+        emails_found = re.findall(r'[\w\.-]+@[\w\.-]+', page_content)
+        return set(emails_found)
+
+    def extract_links(page_content):
+        soup = BeautifulSoup(page_content, 'html.parser')
+        links = soup.find_all('a', class_="lFl0Oc")
+        return [link.get('href') for link in links]
+    
+
+# Modifier votre fonction process_page() pour capturer les exceptions SSL
+    def process_page(href):
+        try:
+            browser.get(href)
+            time.sleep(2)
+            page_content = browser.page_source
+            mentions_legales = re.findall(r'Mentions\s+l[ée]gales', page_content, re.IGNORECASE)
+            if mentions_legales:
+                print("Mentions légales trouvées pour:", href)
+                emails = extract_emails(page_content)
+                for email in emails:
+                    results.append({"url": href, "email": email})
+            else:
+                print("Aucune mention légale trouvée pour:", href)
+                # Si aucune mention légale trouvée, ajouter un résultat avec une indication
+                results.append({"url": href, "email": "Aucune mention légale trouvée"})
+        except Exception as e:
+            print("Erreur lors de l'accès à la page:", e)
+
+
     def Selenium_extractor():
         nonlocal results
         le = 0  # Initialise le compteur
         action = ActionChains(browser)
+        prev_len = None  # Variable pour stocker la longueur précédente de a
+        same_len_count = 0  # Compteur pour suivre le nombre de fois que la longueur de a reste la même
         while True:
             a = browser.find_elements(By.CLASS_NAME, "hfpxzc")
             print(len(a))
-            if len(a) >= 20 or le > 20:
+            # Vérifier si la longueur de a est égale à la longueur précédente
+            if len(a) == prev_len:
+                same_len_count += 1
+            else:
+                prev_len = len(a)
+                same_len_count = 0
+            
+            # Si la longueur de a reste la même pendant trois itérations consécutives
+            if same_len_count >= 3:
                 break
+            
             var = len(a)
             scroll_origin = ScrollOrigin.from_element(a[len(a)-1])
             action.scroll_from_origin(scroll_origin, 0, 30).perform()
             time.sleep(2)
             le += 1
 
-        max_results = 20
+
+        max_results = 300
         results_count = 0
         # Créer un ensemble pour stocker les adresses e-mail uniques
         unique_emails = set()
@@ -93,15 +160,42 @@ def scrape():
     # Ouvrir l'URL dans le navigateur
     browser.get(link)
     time.sleep(10)
-    # Appeler la fonction pour extraire les données
+    try:
+        accept_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.VfPpkd-LgbsSe")))
+        accept_button.click()
+    except Exception as e:
+        print("Erreur lors du clic sur Tout accepter:", e)
+
     Selenium_extractor()
-    # Fermer le navigateur
     browser.quit()
+    results, valid_emails = filter_results(results)
+    results_json = json.dumps(results)  
+    return render_template('index.html', results=results, valid_emails=valid_emails, results_json=results_json, download=True)
 
-    # Retourner les résultats à afficher dans le template HTML
-    print(results)
-    return render_template('index.html', results=results)
 
-# Exécuter l'application Flask
+# Ajoutez une route pour télécharger les résultats au format CSV
+@app.route('/download_csv')
+def download_csv():
+    # Récupérer la chaîne JSON des résultats passés en tant qu'argument
+    results_json = request.args.get('results')
+
+    # Vérifier si des résultats sont disponibles
+    if results_json:
+        # Convertir la chaîne JSON en une liste de dictionnaires
+        results = json.loads(results_json)
+
+        # Créez une chaîne CSV à partir des résultats
+        csv_data = "URL,Email\n"
+        for result in results:
+            csv_data += f"{result['url']},{result['email']}\n"
+
+        # Créez une réponse Flask avec le contenu CSV
+        response = Response(csv_data, mimetype='text/csv')
+        response.headers.set("Content-Disposition", "attachment", filename="results.csv")
+        return response
+    else:
+        return "Aucun résultat à télécharger"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
