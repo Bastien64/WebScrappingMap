6de5d4c4fc60e3dc67de911dc9f5b8d6a01e0718
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-05-03 16:01:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -42,69 +42,35 @@ def filter_results(results):
 def index():
     return render_template('index.html')
 
-
-@app.route('/scrape')
-def scrape():
+# Ajouter une nouvelle route pour gérer le scrolling et le scraping progressif
+@app.route('/scrape_progressive')
+def scrape_progressive():
     search_query = request.args.get('search_query')
     link = f"https://www.google.com/maps/search/{search_query}" 
     chrome_options = Options()
-    chrome_options.add_argument('--ignore-ssl-errors=true')
-    chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    # Initialiser le webdriver Chrome
     browser = webdriver.Chrome(options=chrome_options)
 
-# Liste pour stocker les résultats
     results = []
 
-    def extract_emails(page_content):
-        emails_found = re.findall(r'[\w\.-]+@[\w\.-]+', page_content)
-        return set(emails_found)
-
-    def extract_links(page_content):
-        soup = BeautifulSoup(page_content, 'html.parser')
-        links = soup.find_all('a', class_="lFl0Oc")
-        return [link.get('href') for link in links]
-    
-
-# Modifier votre fonction process_page() pour capturer les exceptions SSL
-    def process_page(href):
-        try:
-            browser.get(href)
-            time.sleep(2)
-            page_content = browser.page_source
-            mentions_legales = re.findall(r'Mentions\s+l[ée]gales', page_content, re.IGNORECASE)
-            if mentions_legales:
-                print("Mentions légales trouvées pour:", href)
-                # Utiliser une expression régulière améliorée pour trouver les adresses e-mail
-                emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', page_content)
-                for email in emails:
-                    results.append({"url": href, "email": email})
-            else:
-                print("Aucune mention légale trouvée pour:", href)
-                # Si aucune mention légale trouvée, ajouter un résultat avec une indication
-                results.append({"url": href, "email": "Aucune mention légale trouvée"})
-        except Exception as e:
-            print("Erreur lors de l'accès à la page:", e)
-
-
-    def Selenium_extractor():
+    # Fonction pour extraire les données de manière progressive
+    def progressive_extraction():
         nonlocal results
-        le = 0  # Initialise le compteur
         action = ActionChains(browser)
-        prev_len = None  # Variable pour stocker la longueur précédente de a
-        same_len_count = 0  # Compteur pour suivre le nombre de fois que la longueur de a reste la même
+        prev_len = None  
+        same_len_count = 0 
+        processed_urls = {}  
+        
         while True:
             a = browser.find_elements(By.CLASS_NAME, "hfpxzc")
-            print(len(a))
-            # Vérifier si la longueur de a est égale à la longueur précédente
             if len(a) == prev_len:
                 same_len_count += 1
             else:
                 prev_len = len(a)
                 same_len_count = 0
-            
-            # Si la longueur de a reste la même pendant trois itérations consécutives
             if same_len_count >= 3:
                 break
             
@@ -112,13 +78,10 @@ def scrape():
             scroll_origin = ScrollOrigin.from_element(a[len(a)-1])
             action.scroll_from_origin(scroll_origin, 0, 30).perform()
             time.sleep(2)
-            le += 1
-
-
-        max_results = 20
+        
+        max_results = 1000
         results_count = 0
-        # Créer un ensemble pour stocker les adresses e-mail uniques
-        unique_emails = set()
+        
         for i in range(len(a)):
             try:
                 scroll_origin = ScrollOrigin.from_element(a[i])
@@ -130,27 +93,43 @@ def scrape():
                 source = browser.page_source
                 soup = BeautifulSoup(source, 'html.parser')
                 links = soup.find_all('a', class_="lcr4fd S9kvJb")
+                
                 for link in links:
                     href = link.get('href')
+                    
+                    if href in processed_urls:
+                        print("URL déjà traitée:", href)
+                        continue
+                    
                     browser.get(href)
                     time.sleep(2)
                     page_content = browser.page_source
                     mentions_legales = re.findall(r'Mentions\s+l[ée]gales', page_content, re.IGNORECASE)
+                    
                     if mentions_legales:
                         print("Mentions légales trouvées pour:", href)
-                        emails_found = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', page_content)
+                        emails_found = set(re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', page_content))
+                        phone_numbers_found = re.findall(r'(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]?\d{2}){4}', page_content)
+
                         for email in emails_found:
-                            # Vérifier si l'e-mail est déjà dans l'ensemble unique_emails
-                            if email not in unique_emails:
-                                unique_emails.add(email)
-                                print("Adresse e-mail trouvée:", email)
-                                # Ajouter l'URL et l'e-mail à la liste des résultats
-                                results.append({"url": href, "email": email})
-                                results_count += 1
+                            phone_number = None
+                            if email not in processed_urls:  
+                                for phone_number in phone_numbers_found:
+                                    if phone_number not in processed_urls.values():  
+                                        processed_urls[href] = phone_number  
+                                        break
+                                else:
+                                    phone_number = None
+                                
+                                if phone_number:  
+                                    results.append({"url": href, "phone_number": phone_number, "email": email})
+                                    results_count += 1
+                                
                                 if results_count >= max_results:
                                     return
                     else:
                         print("Aucune mention légale trouvée pour:", href)
+                        processed_urls[href] = None  
             except StaleElementReferenceException as e:
                 print("Erreur de référence d'élément obsolète. Récupération des éléments à nouveau.")
                 continue
@@ -167,12 +146,14 @@ def scrape():
     except Exception as e:
         print("Erreur lors du clic sur Tout accepter:", e)
 
-    Selenium_extractor()
+    # Appeler la fonction d'extraction progressive
+    progressive_extraction()
     browser.quit()
+
     results, valid_emails = filter_results(results)
     results_json = json.dumps(results)  
+    print(results_json)
     return render_template('index.html', results=results, valid_emails=valid_emails, results_json=results_json, download=True)
-
 
 # Ajoutez une route pour télécharger les résultats au format CSV
 @app.route('/download_csv')
@@ -186,9 +167,9 @@ def download_csv():
         results = json.loads(results_json)
 
         # Créez une chaîne CSV à partir des résultats
-        csv_data = "URL,Email\n"
+        csv_data = "URL,Email,Numero de telephone\n"
         for result in results:
-            csv_data += f"{result['url']},{result['email']}\n"
+            csv_data += f"{result['url']},{result['email']},{result['phone_number']}\n"
 
         # Créez une réponse Flask avec le contenu CSV
         response = Response(csv_data, mimetype='text/csv')
